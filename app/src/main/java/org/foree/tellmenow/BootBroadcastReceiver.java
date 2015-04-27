94bdce9f5d4fa9623ef0fc319a450d7450e440c8
lixiao123 <beijing.2008.lm@163.com>
2015-04-27 19:48:02 +0800

    实现开机自启动功能

--- app/src/main/java/org/foree/tellmenow/BootBroadcastReceiver.java
+++ app/src/main/java/org/foree/tellmenow/BootBroadcastReceiver.java
@@ -4,18 +4,24 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by foree on 15-4-27.
  * receiver boot action to start application
  */
-public class BootBroadcastReceiver extends BroadcastReceiver{
+public class BootBroadcastReceiver extends BroadcastReceiver {
     private static final String TAG = "BootBroadcastReceiver";
+    private static final String action_boot = "android.intent.action.BOOT_COMPLETED";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent phoneService = new Intent(context, PhoneListenerService.class);
-        context.startService(phoneService);
-        Log.v(TAG, "start service");
+        if (intent.getAction().equals(action_boot)) {
+            Intent phoneService = new Intent(context, PhoneListenerService.class);
+            context.startService(phoneService);
+            Log.v(TAG, "start service");
+            //Toast.makeText(context, "start service", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
